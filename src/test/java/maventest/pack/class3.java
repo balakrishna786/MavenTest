4d6acb02eee5a848664c30e0b7f10e5af3430376
DELL <DELL@DESKTOP-S2A13D3>
2019-10-28 20:14:53 +0530

    updated script

---
+++ src/test/java/maventest/pack/class3.java
@@ -0,0 +1,18 @@
+package maventest.pack;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class class3 {
+	@Test
+	public void setUp() {
+		System.setProperty("webdriver.chrome.driver", "E:\\Drivers\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://opensource-demo.orangehrmlive.com/index.php/auth/login");
+		driver.manage().window().maximize();
+		Reporter.log("----------------------class2 executed---------------------------------------",true);
+
+	}
+}
